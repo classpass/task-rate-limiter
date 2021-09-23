37fd7005b5e2a2ec226a0d24c8bf32df1796d202
Drew Stephens <drew.stephens@classpass.com>
2021-09-23 11:54:47 -0400

    Appease Kotlinter

--- src/test/kotlin/com/classpass/jade/ratelimit/TaskRateLimiterTest.kt
+++ src/test/kotlin/com/classpass/jade/ratelimit/TaskRateLimiterTest.kt
@@ -19,7 +19,6 @@ import org.junit.jupiter.api.Test
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.time.Duration
-import java.time.Instant
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.atomic.AtomicReferenceArray
