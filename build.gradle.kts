90121e866a8a228497349594477d5c98e6c59210
Drew Stephens <drew.stephens@classpass.com>
2021-09-27 09:19:50 -0400

    Compile everything to 1.8 bytecode

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,13 @@ tasks {
         useJUnitPlatform()
     }
 
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+
+    compileTestKotlin {
         kotlinOptions {
             jvmTarget = "1.8"
         }
@@ -69,8 +75,8 @@ tasks {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 configure<org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension> {
